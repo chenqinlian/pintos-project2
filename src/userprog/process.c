ffa2962f43616a3206005a3075dd3cbc5ec3149c
chenqinlian <chenqinlian27@gmail.com>
2018-04-25 15:32:30 -0400

    start to write args-parsing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -88,7 +88,7 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
-  while(1);
+  //while(1);
   return -1;
 }
 
@@ -438,7 +438,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE-12;
+        *esp = PHYS_BASE;
       else
         palloc_free_page (kpage);
     }
